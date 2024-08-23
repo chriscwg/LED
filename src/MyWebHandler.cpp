8b6d405b2dd94bfbfd3cb3771e7ae5d5522e1d3d
chriscwg <christoph2.schweiger@audi.de>
2024-08-23 07:52:26 +0200

    better structure - WLAN is hosted

---
+++ src/MyWebHandler.cpp
@@ -0,0 +1,17 @@
+#include "MyWebHandler.h";
+#include <WiFi.h>
+
+WiFiServer server(80);
+const char* ssid = "ESP32_LED";
+const char* password = "kanyewest";
+
+void MyWebHandler::setup(){
+  WiFi.softAP(ssid, password);
+
+  Serial.print("AC: ");
+  Serial.println(WiFi.softAPIP());
+  Serial.print(" | ");
+  Serial.println(password);
+  
+  server.begin();
+}
---
+++ src/MyWebHandler.h
@@ -0,0 +1,12 @@
+#ifndef MYWEBHANDLER_H
+#define MYWEBHANDLER_H
+
+#include <WiFi.h>
+
+
+class MyWebHandler {
+    public:
+        void setup();
+};
+
+#endif
